8a1d1cf617e9d80579a17ed24bd82b5715693191
Francis Abainza <francisabainza714@gmail.com>
2023-09-12 23:17:47 +0800

    Complete project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Shrink-URL
+A capstone project that utilizes a public API
+
+## Installation
+Here is the step-by-step process for running this project on your computer:
+1. Install NodeJS on your system.
+2. Open the command line, then change the directory to this project's directory.
+3. Install nodemon by typing the command `npm i -g nodemon` in the command line.
+4. Install all the dependencies by typing the command `npm i` in the command line.
+5. Run the program by typing the command `nodemon index.js` in the command line.
+6. You can now open the website by searching http://localhost:3000/ using any browser.
---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const port = 3000;
+const app = express();
+const API_URL = "https://cleanuri.com/api/v1/shorten";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    content: ""
+  });
+});
+
+app.post("/post-url", async (req, res) => {
+  try {
+    const result = await axios.post("https://cleanuri.com/api/v1/shorten", req.body);
+    res.render("index.ejs", {
+      content: result.data.result_url
+    })
+  } catch (error) {
+    res.render("index.ejs", {
+      content: error
+    })
+  }
+});
+
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
